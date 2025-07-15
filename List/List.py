c8ee2215eaf83a0ff0f8bdefc04d4a123f84376c
RajeevPatel <rajeev.patel@areteminds.com>
2025-07-15 18:20:20 +0530

    todays changes

--- List/List.py
+++ List/List.py
@@ -3,7 +3,18 @@ friends=["Alice", "Bob", "Charlie", "David", "Eve"];
 friends[2] = friends[2].replace("Charlie", "Rajeev");
 #print(friends)  # Output: ['Alice', 'Bob', 'Rajeev', 'David', 'Eve']
 
-listOfNumbers = [1, 2, 3, 4, 5];
+listOfNumbers = [20,1, 2, 3, 4, 5];
 
-sortList=listOfNumbers.sort(true);
-print(sortList)  # Output: None (sort() modifies the list in place)    
+listOfNumbers.sort();
+#print(listOfNumbers)  # Output: [1, 2, 3, 4, 5] (sort() modifies the list in place)    
+
+listOfNumbers.insert(2,124544)
+
+#print(listOfNumbers)  # Output: [1, 2, 124544, 3, 4, 5, 20] (insert does not change the order of existing elements)
+
+listOfNumbers.pop(2)
+
+#print(listOfNumbers)  # Output: [1, 2, 3, 4, 5, 20] (pop removes the element at the specified position)
+
+listOfNumbers.remove(20)
+print(listOfNumbers)  # Output: [1, 2, 3, 4, 5] (remove removes the first occurrence of the specified value)
