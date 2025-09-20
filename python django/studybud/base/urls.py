fc2df597cd40a1a835a78ebd37d88265a9f7c2be
rajeevpatel8985@gmail.com <rajeev.patel@areteminds.com>
2025-09-20 20:47:25 +0530

    learn extends and include

--- python django/studybud/base/urls.py
+++ python django/studybud/base/urls.py
@@ -1,3 +1,11 @@
+# from django import path
+# from . import views
+
+
+# urlPatterns = [
+#      path('',views.home, name="home"),
+#      path('room/',views.room,name="room"),
+# ]
 from django.urls import path
 from . import views
 
--- python django/studybud/base/views.py
+++ python django/studybud/base/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
 
+rooms = [
+    {"id": 1, "name": "lets learn python"},
+    {"id": 2, "name": "design with pip  learn python"},
+    {"id": 3, "name": "frontend developer learn python"},
+]
+
 #  Create your views here.
 
+
 def home(request):
-    return render(request,'Home.html')
+    context = {"room": rooms}
+    return render(request, "Home.html", context)
+
 
 def room(request):
-    return render(request,'Room.html')
+    return render(request, "Room.html")
